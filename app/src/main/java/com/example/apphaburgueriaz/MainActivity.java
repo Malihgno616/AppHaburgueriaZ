17edce0396a4d104356df8934c4ab1202aade09f
Ackin.Dev <ackinlino123@gmail.com>
2026-03-23 22:53:37 -0300

    Funcionalidade para adicionar quantidade

--- app/src/main/java/com/example/apphaburgueriaz/MainActivity.java
+++ app/src/main/java/com/example/apphaburgueriaz/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.apphaburgueriaz;
 
-import android.os.Bundle;import android.widget.CheckBox;import android.widget.EditText;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Double priceBurguer = 20.00;
+
+    public Integer quantity = 0;
+
+    public Double totalPrice = 0.00;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,43 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TextView txtQuantity = findViewById(R.id.quantity);
+
+        EditText orderInput = findViewById(R.id.pedidoInput);
+
+        CheckBox[] checkBoxes= {
+                findViewById(R.id.checkBacon),
+                findViewById(R.id.checkCheese),
+                findViewById(R.id.checkOnion),
+                findViewById(R.id.checkKetchup),
+                findViewById(R.id.checkBbq)
+        };
+
+        Button add = findViewById(R.id.buttonPlus);
+
+        Button sub = findViewById(R.id.buttonSub);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                quantity++;
+                txtQuantity.setText(String.valueOf(quantity));
+            }
+        });
+
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                quantity--;
+
+                if(quantity < 0) {
+                    quantity = 0;
+                }
+
+                txtQuantity.setText(String.valueOf(quantity));
+            }
         });
     }
 
