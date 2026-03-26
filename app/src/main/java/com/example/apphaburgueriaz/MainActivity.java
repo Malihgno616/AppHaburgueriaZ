3a87b96c3b518673a3c82db2a97c09077f383ae2
Ackin.Dev <ackinlino123@gmail.com>
2026-03-25 21:34:25 -0300

    Gerando o resumo do pedido em base na quantidade de lanches (no momento só exibindo o nome)

--- app/src/main/java/com/example/apphaburgueriaz/MainActivity.java
+++ app/src/main/java/com/example/apphaburgueriaz/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.apphaburgueriaz;
 
 import android.os.Bundle;
-import android.util.Log;  // IMPORTANTE: adicionar esta importação
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;  // Para mensagens popup
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -17,23 +16,26 @@ import androidx.core.view.WindowInsetsCompat;
 interface Components {
     void startComponents();
     void calculate();
+    void sendMail();
+    void showPreview();
 }
 
 public class MainActivity extends AppCompatActivity implements Components {
     public double priceBurguer = 20.00;
     public double[] prices = {5.00, 4.00, 7.00, 3.00, 4.50};
-    public int quantity = 1;
+    public int quantity = 0;
     public double totalPrice = 0.00;
     public TextView txtQuantity;
     public TextView txtTotalPrice;
-    public EditText orderInput;
+    public EditText clientInput;
     public CheckBox[] checkBoxes;
+    public TextView[] previews;
 
     @Override
     public void startComponents() {
         txtQuantity = findViewById(R.id.quantity);
         txtTotalPrice = findViewById(R.id.totalPrice);
-        orderInput = findViewById(R.id.pedidoInput);
+        clientInput = findViewById(R.id.clientInput);
         checkBoxes = new CheckBox[] {
                 findViewById(R.id.checkBacon),
                 findViewById(R.id.checkCheese),
@@ -41,6 +43,21 @@ public class MainActivity extends AppCompatActivity implements Components {
                 findViewById(R.id.checkKetchup),
                 findViewById(R.id.checkBbq)
         };
+
+        previews = new TextView[] {
+                findViewById(R.id.namePreview),
+                findViewById(R.id.baconPreview),
+                findViewById(R.id.cheesePreview),
+                findViewById(R.id.onionPreview),
+                findViewById(R.id.ketchupPreview),
+                findViewById(R.id.bbqPreview),
+                findViewById(R.id.qttPreview),
+                findViewById(R.id.totalPreview)
+        };
+
+        for(int i = 0; i < previews.length; i++) {
+            previews[i].setVisibility(View.GONE);
+        }
 
         for(int i = 0; i < checkBoxes.length; i++) {
             checkBoxes[i].setOnClickListener(new View.OnClickListener() {
@@ -63,6 +80,8 @@ public class MainActivity extends AppCompatActivity implements Components {
                 quantity++;
                 txtQuantity.setText(String.valueOf(quantity));
                 calculate();
+                showPreview();
+
             }
         });
 
@@ -92,6 +111,31 @@ public class MainActivity extends AppCompatActivity implements Components {
 
             totalPrice += priceBurguer * quantity;
             txtTotalPrice.setText(String.format("R$ %.2f", totalPrice));
+    }
+
+    @Override
+    public void showPreview() {
+        if(quantity > 0) {
+            for (int i = 0; i < previews.length; i++) {
+                previews[i].setVisibility(View.VISIBLE);
+            }
+
+            String clientName = clientInput.getText().toString().trim();
+            if(clientName.isEmpty()) {
+                clientName = "Cliente não informado";
+            }
+            previews[0].setText("Nome do cliente: " + clientName);
+
+        } else {
+            for (int i = 0; i < previews.length; i++) {
+                previews[i].setVisibility(View.GONE);
+            }
+        }
+    }
+
+    @Override
+    public void sendMail() {
+
     }
 
     @Override
